6d690a4f4c007a85d6512d6ee6c366ebfd71ea11
mari-trancoso <mnegraotrancoso@gmail.com>
2022-09-13 13:55:01 -0300

    Exercicios feitos

---
+++ exercicio2/script.js
@@ -0,0 +1,11 @@
+let numero = Number(prompt("Digite um número para saber a sua tabuada de 1 a 10."))
+let xxx = ['' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,'' ,]
+
+for (let i in xxx){
+    let tabuada = (Number(i)+1)*numero
+    console.log(tabuada)
+}
+
+// for (let i =2; i<=20; i+=2){
+//     console.log(i)
+// }
